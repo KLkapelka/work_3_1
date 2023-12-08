47fbb1eaaf3aca8547f2f9b97a3a550ac0e12c97
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 06:18:08 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class work_3_1
+{
+    static void Main()
+    {
+
+        string[] lines = File.ReadAllLines("input.txt"); 
+        int[] conditions = lines[0].Split(' ').Select(int.Parse).ToArray(); 
+        int n = int.Parse(lines[1]); 
+        string[] tickets = lines.Skip(2).ToArray(); 
+
+        using (StreamWriter writer = new StreamWriter("output.txt"))
+        {
+            foreach (var i in tickets) 
+            {
+                int[] ticketNumbers = i.Split(' ').Select(int.Parse).ToArray(); 
+                int count = ticketNumbers.Count(num => conditions.Contains(num));
+
+                if (count >= 3)
+                    writer.WriteLine("Lucky");
+                else
+                    writer.WriteLine("Unlucky"); 
+            }
+        }
+    }
+}
